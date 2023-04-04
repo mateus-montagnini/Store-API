18e6b3423144ad6fbb149d7c6c9064676ecc0cb3
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-04-04 16:19:00 -0300

    cria arquivo app.js

---
+++ app.js
@@ -0,0 +1,42 @@
+// 'g  G  h  H ""'
+require('dotenv').config();
+
+require('express-async-errors');
+
+const express = require('express');
+const app = express();
+
+const connectDB = require('./db/connect');
+const productsRouter = require('./routes/products');
+
+const notFoundMiddleware = require('./middleware/not-found');
+const errorMiddleware = require('./middleware/error-handler');
+
+// middleware
+app.use(express.json());
+
+// routes
+
+app.get('/', (req, res) => {
+    res.send('<1>Store API</1><a href="/api/v1/products">products route</a>')
+})
+
+app.use('/api/v1/products', productsRouter);
+
+// products route
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const PORT = process.env.PORT || 3000;
+
+const start = async () => {
+    try {
+    await connectDB(process.env.MONGO_URI)
+    app.listen(PORT, console.log(`Listening on port: ${PORT}`))
+    } catch (error) {
+        
+    }
+}
+
+start();
